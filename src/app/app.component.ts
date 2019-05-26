0169385ce514e1e07b5c6e8e532545fe7a82a88a
Shubham <shbh29@gmail.com>
2019-05-26 23:07:31 +0530

    turned into angular project and simple h1 tag

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'shbh29.github.io';
+  name = 'Singh Shubham Dilip';
+}
